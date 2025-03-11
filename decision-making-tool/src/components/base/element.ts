ddf969fe6e3d805e87297c19710449d35b40245e
dusixx <113414935+dusixx@users.noreply.github.com>
2025-03-11 17:44:04 +0200

    feat: implement Element

---
+++ decision-making-tool/src/components/base/element.ts
@@ -0,0 +1,51 @@
+import { BaseElement } from './base-element';
+
+export class Element extends BaseElement {
+  public get visible(): boolean {
+    const { style } = this.node;
+    return style.visibility === 'visible';
+  }
+
+  public set visible(flag: boolean) {
+    const { style } = this.node;
+    style.visibility = flag ? 'visible' : 'hidden';
+    style.pointerEvents = flag ? '' : 'none';
+  }
+
+  public dispatch(eventType: string, options: EventInit): boolean {
+    return this.node.dispatchEvent(
+      new Event(eventType, {
+        bubbles: true,
+        cancelable: true,
+        ...options,
+      })
+    );
+  }
+
+  public dispatchCustom(
+    eventType: string,
+    detail: Record<string, unknown>,
+    options: EventInit
+  ): boolean {
+    return this.node.dispatchEvent(
+      new CustomEvent(eventType, {
+        bubbles: true,
+        cancelable: true,
+        detail: { ...detail, target: this },
+        ...options,
+      })
+    );
+  }
+
+  public allowPointerEvents(flag: boolean): void {
+    this.node.style.pointerEvents = flag ? '' : 'none';
+  }
+
+  public hide(): void {
+    this.node.style.display = 'none';
+  }
+
+  public show(): void {
+    this.node.style.display = '';
+  }
+}
