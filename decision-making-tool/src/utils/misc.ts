f58cc26511bcccbc47e5f896c691c1ca1ada8bfc
dusixx <113414935+dusixx@users.noreply.github.com>
2025-03-14 04:27:27 +0200

    feat: update utils

--- decision-making-tool/src/utils/misc.ts
+++ decision-making-tool/src/utils/misc.ts
@@ -10,11 +10,34 @@ export function JSONParse(data: string): unknown {
   }
 }
 
-export function saveToJSONFile(content: string, fileName: string): void {
-  const link = document.createElement('a');
-  const file = new Blob([content], { type: 'text/plain' });
+export const isPositiveInt = (v: number | string): boolean => {
+  return Number.isInteger(typeof v === 'number' ? v : parseFloat(v));
+};
 
-  link.href = URL.createObjectURL(file);
-  link.download = `${fileName}.json`;
-  link.click();
-}
+export const rndInt = (min: number, max: number): number => {
+  return Math.round(min + Math.random() * (max - min));
+};
+
+type ColorRGBHex = { rgb: number[]; hex: string };
+
+export const getRandomColor = ({ min = 0, max = 255 } = {}): ColorRGBHex => {
+  const rgb = [0, 0, 0].map(() => rndInt(min, max));
+  const hex = rgb.map((v) => v.toString(16).padStart(2, '0'));
+
+  return {
+    rgb,
+    hex: `#${hex.join('')}`,
+  };
+};
+
+export const getColorMixCss = ({
+  min = 25,
+  max = 30,
+  percent = 50,
+  baseColor = '#ffcfcf',
+} = {}): string => {
+  const randomInt = rndInt(min, max).toString();
+  const colorHex = getRandomColor().hex;
+
+  return `color-mix(in oklab, ${colorHex} ${randomInt}%, ${baseColor} ${percent.toString()}%)`;
+};
