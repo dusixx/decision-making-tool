88af5960828ec1d456413cf1f36213a2d88b9799
dusixx <113414935+dusixx@users.noreply.github.com>
2025-03-21 16:30:38 +0200

    fix: update Wheel

---
+++ decision-making-tool/src/components/wheel/utils/misc.ts
@@ -0,0 +1,59 @@
+import { getRndColorMixCss } from '../../../utils/color.ts';
+import { randomizeArray, rndInt } from '../../../utils/misc.ts';
+import type { OptionData } from '../../option-list/option-list.ts';
+import type { SliceData } from '../wheel.ts';
+
+export const PI2 = Math.PI * 2;
+const CURSOR_POSITION_RAD = Math.PI * 1.5;
+
+const WHEEL_SPIN_SPEED_MIN = 20;
+const WHEEL_SPIN_SPEED_MAX = 35;
+const WHEEL_SPIN_SPEED_RATIO = 1e5;
+
+export const createSlicesFromOptions = (
+  options: OptionData[],
+  totalWeight: number
+): SliceData[] => {
+  const slices: SliceData[] = [];
+  let startAngleRad: number = 0;
+  const randomizedOptions = randomizeArray<OptionData>(options);
+
+  for (const item of randomizedOptions) {
+    const itemAngleRad = (PI2 * item.weight) / totalWeight;
+    const endAngleRad = startAngleRad + itemAngleRad;
+
+    const slice = {
+      ...item,
+      startAngleRad,
+      endAngleRad,
+      color: getRndColorMixCss(),
+    };
+    slices.push(slice);
+
+    startAngleRad = endAngleRad;
+  }
+  return slices;
+};
+
+export const getRndWheelSpeed = (
+  min: number = WHEEL_SPIN_SPEED_MIN,
+  max: number = WHEEL_SPIN_SPEED_MAX
+): number => {
+  return rndInt(min, max) / WHEEL_SPIN_SPEED_RATIO;
+};
+
+export const isCurrentSlice = (slice: SliceData): boolean => {
+  return CURSOR_POSITION_RAD <= slice.endAngleRad && CURSOR_POSITION_RAD >= slice.startAngleRad;
+};
+
+export const updateSliceAngles = (slice: SliceData, angleDeltaRad: number): void => {
+  let { startAngleRad: start, endAngleRad: end } = slice;
+
+  start = (start + angleDeltaRad) % PI2;
+  end = (end + angleDeltaRad) % PI2;
+  if (end < start) {
+    end += PI2;
+  }
+  slice.startAngleRad = start;
+  slice.endAngleRad = end;
+};
--- decision-making-tool/src/components/wheel/wheel.ts
+++ decision-making-tool/src/components/wheel/wheel.ts
@@ -1,38 +1,44 @@
-import { getRndColorMixCss, randomizeArray } from '../../utils/index.ts';
+import { getRndColorMixCss } from '../../utils/index.ts';
 import { Element } from '../base/element.ts';
 import type { OptionData } from '../option-list/option-list.ts';
-import type { ParseResult } from './helpers.ts';
-import { getRndWheelSpeed, parseOptionsData, VALID_ITEMS_COUNT } from './helpers.ts';
+import {
+  PI2,
+  createSlicesFromOptions,
+  getRndWheelSpeed,
+  isCurrentSlice,
+  updateSliceAngles,
+} from './utils/misc.ts';
 
 import styles from './wheel.module.scss';
 
-const PI2 = Math.PI * 2;
-const CANVAS_PADDING = 20;
-const LINE_WIDTH = 1;
+const CANVAS_PADDING = 30;
+const LINE_WIDTH = 1.5;
 const STROKE_COLOR = 'white';
 
-const NEEDLE_RADIUS_RATIO = 0.13;
+const NEEDLE_RADIUS_RATIO = 0.15;
 const NEEDLE_COLOR = 'white';
 
-const WHEEL_DEF_RADIUS = 250;
+const WHEEL_DEFAULT_RADIUS = 250;
 
-const CURSOR_POSITION = Math.PI * 1.5;
 const CURSOR_COLOR = 'rgb(206, 20, 104)';
+const CURSOR_LINE_WIDTH = 1.5;
+const SHADOW_BLUR = 4;
+const SHADOW_COLOR = 'rgb(170 170 170)';
 
-const SLICE_TEXT_NEEDLE_OFFSET = 30;
+const SLICE_TEXT_NEEDLE_OFFSET = 25;
 const SLICE_TEXT_VISIBILITY_ANGLE_THRESHOLD = 0.27;
-const SLICE_TEXT_COLOR = 'black';
-const SLICE_TEXT_FONT = '16px sans-serif';
+const SLICE_TEXT_COLOR = 'rgb(40 40 40)';
+const SLICE_TEXT_FONT = '15px sans-serif';
+const SLICE_TEXT_BASELINE = 'middle';
 
 const ERR_INVALID_CONTEXT =
   'The context id is not supported, or the canvas has already been set to a different context mode';
-
-const ERR_INVALID_OPTIONS_COUNT = `There must be at least ${VALID_ITEMS_COUNT.toString()} options`;
 
 export type SliceData = OptionData & {
   startAngleRad: number;
   endAngleRad: number;
   color: string;
+  sliceText?: string;
 };
 
 type Point = { x: number; y: number };
@@ -41,45 +47,28 @@ type OnSlideChangeHandler = ((currentSlice: SliceData | null) => void) | null;
 
 type Props = {
   options: OptionData[];
+  totalWeight: number;
   radius?: number;
   duration?: number;
 };
 
-const isCurrentSlice = (slice: SliceData): boolean => {
-  let { startAngleRad, endAngleRad } = slice;
-  startAngleRad = startAngleRad > PI2 ? startAngleRad % PI2 : startAngleRad;
-  endAngleRad = endAngleRad > PI2 ? endAngleRad % PI2 : endAngleRad;
-
-  return CURSOR_POSITION <= endAngleRad && CURSOR_POSITION >= startAngleRad;
-};
-
-//
-//-----------------------------
-// Wheel
-//-----------------------------
-//
-
 export class Wheel extends Element<HTMLCanvasElement> {
   private context: CanvasRenderingContext2D;
   private slices: SliceData[] = [];
-  private totalWeight: number = 0;
   private _radius: number = 0;
   private _onFinish: OnSlideChangeHandler = null;
   private _onChange: OnSlideChangeHandler = null;
   private _currentSlice: SliceData | null = null;
   private _abort = false;
 
-  constructor({ options, radius = WHEEL_DEF_RADIUS }: Props) {
+  constructor({ options, totalWeight, radius = WHEEL_DEFAULT_RADIUS }: Props) {
     super({ tag: 'canvas', className: styles.canvas });
 
     this.radius = radius;
     this.context = this.getContext2D();
+    this.slices = createSlicesFromOptions(options, totalWeight);
 
-    const parseResult = this.parseOptions(options);
-    this.createSlicesFromOptions(parseResult.validOptions);
-
-    this.createNeedle();
-    this.createCursor();
+    this.draw();
   }
 
   public get currentSlice(): SliceData | null {
@@ -144,6 +133,7 @@ export class Wheel extends Element<HTMLCanvasElement> {
         return;
       }
       this.draw(angle);
+
       rafId = requestAnimationFrame(animate);
     };
 
@@ -152,72 +142,31 @@ export class Wheel extends Element<HTMLCanvasElement> {
 
   public repaint(): void {
     this.clearCanvas();
+    this.createBaseCircle();
 
     for (const slice of this.slices) {
       slice.color = getRndColorMixCss();
-      this.makeSlice(slice);
+      this.createSlice(slice);
     }
-  }
-
-  private makeSlice(slice: SliceData): void {
-    this.createSlice(slice);
     this.createNeedle();
     this.createCursor();
   }
 
-  private setCurrentSlice(slice: SliceData): void {
-    if (isCurrentSlice(slice)) {
-      this._currentSlice = slice;
-      this._onChange?.(slice);
-    }
-  }
-
   private draw(angleDeltaRad: number = 0): void {
     this.clearCanvas();
+    this.createBaseCircle();
 
     for (const slice of this.slices) {
-      slice.startAngleRad += angleDeltaRad;
-      slice.endAngleRad += angleDeltaRad;
+      updateSliceAngles(slice, angleDeltaRad);
 
-      this.setCurrentSlice(slice);
-      this.makeSlice(slice);
-    }
-  }
-
-  private parseOptions(options: OptionData[]): ParseResult {
-    const result = parseOptionsData(options);
-    if (!result.isValid) {
-      throw RangeError(ERR_INVALID_OPTIONS_COUNT);
-    }
-    this.totalWeight = result.totalWeight;
-
-    return result;
-  }
-
-  private createSlicesFromOptions(options: OptionData[]): void {
-    this.clearCanvas();
-    this.slices = [];
-
-    let startAngleRad: number = 0;
-    const randomizedOptions = randomizeArray<OptionData>(options);
-
-    for (const item of randomizedOptions) {
-      const itemAngleRad = (PI2 * item.weight) / this.totalWeight;
-      const endAngleRad = startAngleRad + itemAngleRad;
-
-      const slice = {
-        ...item,
-        startAngleRad,
-        endAngleRad,
-        color: getRndColorMixCss(),
-      };
-
-      this.setCurrentSlice(slice);
+      if (isCurrentSlice(slice)) {
+        this._currentSlice = slice;
+        this._onChange?.(slice);
+      }
       this.createSlice(slice);
-      this.slices.push(slice);
-
-      startAngleRad = endAngleRad;
     }
+    this.createNeedle();
+    this.createCursor();
   }
 
   private clearCanvas(): void {
@@ -239,7 +188,7 @@ export class Wheel extends Element<HTMLCanvasElement> {
 
     // text too long
     if (ratio <= 1) {
-      const shortenedTitle = title.slice(0, Math.floor(title.length * ratio) - 2);
+      const shortenedTitle = title.slice(0, Math.floor(title.length * ratio) - 5);
       return `${shortenedTitle}...`;
     }
     return title;
@@ -250,13 +199,19 @@ export class Wheel extends Element<HTMLCanvasElement> {
       context,
       center: { x: cx },
     } = this;
-    context.beginPath();
-    context.moveTo(cx - 10, 0);
-    context.lineTo(cx, 5);
-    context.lineTo(cx + 10, 0);
-    context.lineTo(cx, 25);
+
+    context.lineWidth = CURSOR_LINE_WIDTH;
+    context.strokeStyle = STROKE_COLOR;
     context.fillStyle = CURSOR_COLOR;
+
+    context.beginPath();
+    context.moveTo(cx - 14, 0);
+    context.lineTo(cx, 6);
+    context.lineTo(cx + 14, 0);
+    context.lineTo(cx, 27);
+    context.closePath();
     context.fill();
+    context.stroke();
   }
 
   private createNeedle(): void {
@@ -265,31 +220,38 @@ export class Wheel extends Element<HTMLCanvasElement> {
       radius,
       center: { x: cx, y: cy },
     } = this;
+
+    context.shadowBlur = SHADOW_BLUR;
+    context.shadowColor = SHADOW_COLOR;
+
     context.beginPath();
     context.arc(cx, cy, radius * NEEDLE_RADIUS_RATIO, 0, PI2);
     context.fillStyle = NEEDLE_COLOR;
     context.lineTo(cx, cy);
     context.fill();
+    context.closePath();
+
+    this.context.shadowColor = 'transparent';
   }
 
-  private createSliceText(slice: SliceData): void {
-    const { startAngleRad, endAngleRad, title } = slice;
+  private createBaseCircle(): void {
     const {
       context,
+      radius,
       center: { x: cx, y: cy },
     } = this;
 
-    context.save();
-    context.textBaseline = 'middle';
-    context.translate(cx, cy);
-    context.rotate((startAngleRad + endAngleRad) / 2);
+    context.shadowBlur = SHADOW_BLUR + 1;
+    context.shadowColor = SHADOW_COLOR;
+    context.fillStyle = NEEDLE_COLOR;
 
-    context.fillStyle = SLICE_TEXT_COLOR;
-    context.font = SLICE_TEXT_FONT;
+    context.beginPath();
+    context.arc(cx, cy, radius, 0, PI2);
+    context.lineTo(cx, cy);
+    context.fill();
+    context.closePath();
 
-    const slicedTitle = this.fitSliceText(title);
-    context.fillText(slicedTitle, this.needleRadius + SLICE_TEXT_NEEDLE_OFFSET, 0);
-    context.restore();
+    this.context.shadowColor = 'transparent';
   }
 
   private createSlice(slice: SliceData): void {
@@ -302,10 +264,11 @@ export class Wheel extends Element<HTMLCanvasElement> {
 
     const sliceAngleRad = endAngleRad - startAngleRad;
 
-    context.beginPath();
     context.lineWidth = LINE_WIDTH;
     context.strokeStyle = STROKE_COLOR;
     context.fillStyle = color;
+
+    context.beginPath();
     context.moveTo(cx, cy);
     context.arc(cx, cy, radius, startAngleRad, endAngleRad);
     context.closePath();
@@ -316,5 +279,28 @@ export class Wheel extends Element<HTMLCanvasElement> {
     if (sliceAngleRad >= SLICE_TEXT_VISIBILITY_ANGLE_THRESHOLD) {
       this.createSliceText(slice);
     }
+  }
+
+  private createSliceText(slice: SliceData): void {
+    const { startAngleRad, endAngleRad, title } = slice;
+    const {
+      context,
+      center: { x: cx, y: cy },
+    } = this;
+
+    context.lineWidth = LINE_WIDTH;
+    context.fillStyle = SLICE_TEXT_COLOR;
+    context.font = SLICE_TEXT_FONT;
+    context.textBaseline = SLICE_TEXT_BASELINE;
+
+    context.save();
+    context.translate(cx, cy);
+    context.rotate((startAngleRad + endAngleRad) / 2);
+
+    slice.sliceText = slice.sliceText || this.fitSliceText(title);
+
+    //context.strokeText(text, this.needleRadius + SLICE_TEXT_NEEDLE_OFFSET, 0);
+    context.fillText(slice.sliceText, this.needleRadius + SLICE_TEXT_NEEDLE_OFFSET, 0);
+    context.restore();
   }
 }
